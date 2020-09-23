e9f57c8b1a6faa6b91827e2446c072dd1b46ca2e
Saw Soe <sawsoemoenyunt@connect.qut.edu.au>
2020-09-23 11:26:07 +1000

    #added PlanListViewPage, PlanListModel

---
+++ Uniq/ViewModels/PlanListViewModel.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Windows.Input;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using System.Collections.ObjectModel;
+using Uniq.Models;
+using Uniq.Views;
+
+namespace Uniq.ViewModels
+{
+    public class PlanListViewModel : BaseViewModel
+    {
+        public ObservableCollection<Announcement> Plans { get; set; }
+
+        public PlanListViewModel()
+        {
+            SetupData();
+
+            Title = "Plans";
+        }
+
+        void SetupData()
+        {
+            Plans = new ObservableCollection<Announcement>()
+            {
+                new Announcement
+                {
+                    Name = "Meet with John Smith",
+                    Unit = "CAB202",
+                    Content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+                    ProfileImage = "tab_about.png",
+                    TimePosted = "Tue 11:32am"
+                },
+                new Announcement
+                {
+                    Name = "Meet with John Smith",
+                    Unit = "CAB202",
+                    Content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+                    ProfileImage = "tab_about.png",
+                    TimePosted = "Tue 11:32am"
+                },
+                new Announcement
+                {
+                    Name = "Meet with John Smith",
+                    Unit = "CAB202",
+                    Content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+                    ProfileImage = "tab_about.png",
+                    TimePosted = "Tue 11:32am"
+                },
+                new Announcement
+                {
+                    Name = "Meet with John Smith",
+                    Unit = "CAB202",
+                    Content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+                    ProfileImage = "tab_about.png",
+                    TimePosted = "Tue 11:32am"
+                },
+                new Announcement
+                {
+                    Name = "Meet with John Smith",
+                    Unit = "CAB202",
+                    Content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+                    ProfileImage = "tab_about.png",
+                    TimePosted = "Tue 11:32am"
+                }
+            };
+        }
+    }
+}
