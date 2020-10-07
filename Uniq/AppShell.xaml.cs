3c3a67e840673d12cf90c6c7ac62b04cf2e5fde4
Eduard-Volobuev <69882649+Eduard-Volobuev@users.noreply.github.com>
2020-10-07 15:57:20 +1000

    Support Page Routes

--- Uniq/AppShell.xaml.cs
+++ Uniq/AppShell.xaml.cs
@@ -13,6 +13,11 @@ namespace Uniq
             InitializeComponent();
 
             Routing.RegisterRoute(nameof(AnnouncementsPage), typeof(AnnouncementsPage));
+            Routing.RegisterRoute(nameof(TalkPage), typeof(TalkPage));
+            Routing.RegisterRoute(nameof(ContactPage), typeof(ContactPage));
+            Routing.RegisterRoute(nameof(ExtensionPage), typeof(ExtensionPage));
+            Routing.RegisterRoute(nameof(AppointmentPage), typeof(AppointmentPage));
+            Routing.RegisterRoute(nameof(ManualPage), typeof(ManualPage));
         }
 
     }
