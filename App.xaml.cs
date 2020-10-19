ac6009465fb37a945edeef433a9d719e0db6fcfe
ljackson140 <62328065+ljackson140@users.noreply.github.com>
2020-10-19 20:03:35 +1000

    Add files via upload
    lecture tab

---
+++ App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Uniq.Services;
+using Uniq.Views;
+
+namespace Uniq
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        public static string User = "Rendy";
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
